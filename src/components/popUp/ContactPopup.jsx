7dededf2e00da281c46fb3b00f5a34f98242ee87
haidihassan <haidihassan99@yahoo.com>
2024-09-25 23:15:36 +0300

    change in pop up

--- src/components/popUp/ContactPopup.jsx
+++ src/components/popUp/ContactPopup.jsx
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Popup.css";
 
-const ContactPopup = ({ currentContact, contact, onClose }) => {
-  if (!contact) return null; 
+const ContactPopup = ({  contact, onClose }) => {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    if (contact) {
+      dialogRef.current?.showModal(); 
+    } else {
+      dialogRef.current?.close(); 
+    }
+  }, [contact]);
 
   return (
-    <div className="popup-overlay">
-      <div className="popup-content">
-        <h2 style={{ textAlign: "center" }}>Contact Details</h2>
+    <dialog ref={dialogRef} className="popup-content">
+      <h2 style={{ textAlign: "center" }}>Contact Details</h2>
+      <p style={{ marginLeft: "15px" }}>
+        <strong>Name:</strong> {contact?.name}
+      </p>
+      <p style={{ marginLeft: "15px" }}>
+        <strong>Email:</strong> {contact?.email}
+      </p>
+      <p style={{ marginLeft: "15px" }}>
+        <strong>Contact Type:</strong> {contact?.contactType}
+      </p>
+      <p style={{ marginLeft: "15px" }}>
+        <strong>Job Title:</strong> {contact?.jobTitle}
+      </p>
+      {contact?.notes && (
         <p style={{ marginLeft: "15px" }}>
-          <strong>Name:</strong> {contact.name}
+          <strong>Notes:</strong> {contact.notes}
         </p>
-        <p style={{ marginLeft: "15px" }}>
-          <strong>Email:</strong> {contact.email}
-        </p>
-        <p style={{ marginLeft: "15px" }}>
-          <strong>Contact Type:</strong> {contact.contactType}
-        </p>
-        <p style={{ marginLeft: "15px" }}>
-          <strong>Job Title:</strong> {contact.jobTitle}
-        </p>
-        {contact?.notes && (
-          <p style={{ marginLeft: "15px" }}>
-            <strong>Notes:</strong> {contact.notes}
-          </p>
-        )}
-        <button onClick={onClose}>Close</button>
-      </div>
-    </div>
+      )}
+      <button onClick={onClose}>Close</button>
+    </dialog>
   );
 };
 
